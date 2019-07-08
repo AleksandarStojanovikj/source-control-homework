c5f983a2a304808a3783eeea60c8bf90296f8abe
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2019-07-08 15:37:05 +0200

    Add initial implementation

--- src/SomeStuff.java
+++ src/SomeStuff.java
@@ -1,2 +1,12 @@
 public class SomeStuff {
+    private final String HOMEWORK_STRING = "HOMEWORK";
+    private int someNumber;
+
+    public SomeStuff(int someNumber) {
+        this.someNumber = someNumber;
+    }
+
+    public void printSomething() {
+        System.out.println(HOMEWORK_STRING);
+    }
 }
